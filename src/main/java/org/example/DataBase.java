b9f8150dd9262e47e6ea0d75bb4720cdf487bc1d
Alexandru Corban <alexcorban59@yahoo.com>
2024-08-06 19:39:51 +0300

    Bank Project v1.0

--- src/main/java/org/example/DataBase.java
+++ src/main/java/org/example/DataBase.java
@@ -2,12 +2,13 @@ package org.example;
 import java.sql.*;
 
 public class DataBase {
+    // Connection to the Database
     public Connection getConnection(String dbname, String user, String pass) {
-        Connection con = null;
+        Connection conn = null;
         try {
             Class.forName("org.postgresql.Driver");
-            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
-            if (con != null) {
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
+            if (conn != null) {
                 System.out.println("Connected to PostgreSQL database");
             } else {
                 System.out.println("Failed to connect to PostgreSQL database");
@@ -15,13 +16,52 @@ public class DataBase {
         } catch (Exception e) {
             System.out.println(e);
         }
-        return con;
+        return conn;
     }
 
+    // Load users from table
+    public void loadUser(Connection conn, String tableName) {
+        Statement statement;
+        ResultSet rs;
+        try {
+            String query = String.format("select * from %s", tableName);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.print(rs.getInt("id") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.print(rs.getShort("pin") + " ");
+                System.out.println(rs.getDouble("sold") + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void searchUserByName(Connection conn, String tableName, String name) {
+        Statement statement;
+        ResultSet rs;
+        try {
+            String query = String.format("select * from %s where name = '%s'", tableName, name);
+            statement = conn.createStatement();
+            rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.print(rs.getInt("id") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.print(rs.getShort("pin") + " ");
+                System.out.println(rs.getDouble("sold") + " ");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    // Add an user to the table
     public void addUser(Connection conn, String tableName, String name, short pin, double sold) {
         Statement statement;
         try {
-            String query = String.format("Insert into %s(name, pin, sold) values('%s','%d','%f')", tableName, name, pin, sold);
+            String query = String.format("insert into %s(name, pin, sold) values('%s','%d','%f')", tableName, name, pin, sold);
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.printf("User %s added to table %s.", name, tableName);
@@ -29,4 +69,18 @@ public class DataBase {
             System.out.println(e);
         }
     }
+
+    // Remove an user to the table
+    public void deleteUser(Connection conn, String tableName, int id) {
+        Statement statement;
+        try {
+            String query = String.format("delete from %s where id = '%d'", tableName, id);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.printf("Id %d deleted from table %s.", id, tableName);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,22 @@
 package org.example;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         DataBase db = new DataBase();
 
-       Connection con = db.getConnection("UsersDB", "postgres", "12345");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the password for Database: ");
+        String Password;
+        Password = sc.nextLine();
 
-        db.addUser(con, "users","Teodoro Emil", (short)4534, 12045.65);
+        Connection conn = db.getConnection("UsersDB", "postgres", Password);
+
+        db.loadUser(conn, "users");
+        db.searchUserByName(conn, "users", "Corban Alexandru");
 
         }
     }
